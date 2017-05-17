e889bff872b91001325dd84322dcc78feff2225c
kiwibatave <b_cuzon@yahoo.fr>
2017-05-17 11:47:49 +0200

    blur added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
     // Apparition menu
 
       $(document).ready(function () {
-          
+
       $(".bbjmenu2").click(function() {
         $(".bbjcontainer").css({display: 'block'});
+      $("body").css({filter: 'blur(5px)'});
       });
 
       $(".croix").click(function() {
 
         $(".bbjcontainer").css({display: 'none'});
+        $("body").css({filter: 'none'});
       });
 });
-   
-    
